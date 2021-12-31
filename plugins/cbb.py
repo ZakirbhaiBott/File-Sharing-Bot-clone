93cf3ce475a01f78d9415bed33b1eacd2d5fcaa3
Vasubai <90282707+Vasubai@users.noreply.github.com>
2022-01-01 01:59:59 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Creator : <a href='https://t.me/mc_serie'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ MOVIES : <a href='https://t.me/joinchat/Fhcssgw5H8wwMDBl'>Click here</a>\n○ Movie Group : @malayayalies\n○ Updates : @malayalam2</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
