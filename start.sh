b6625f5e7056409898138f79e6de360616d74fe3
ZakirbhaiBott <87867737+ZakirbhaiBott@users.noreply.github.com>
2022-01-01 10:12:19 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/ZakirbhaiBott/File-Sharing-Bot-clone.git /EvaMaria
+  git clone https://github.com/ZakirbhaiBott/File-Sharing-Bot-clone.git /File-Sharing-Bot-clone
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /File-Sharing-Bot-clone
